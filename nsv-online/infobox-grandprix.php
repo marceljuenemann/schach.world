7dcc0364ef98573f6f948e5cf6d8680182279021
Marcel Juenemann <webmaster@nsv-online.de>
2023-04-01 02:50:14 +0200

    .gitignore file to match actual production server

---
+++ nsv-online/infobox-grandprix.php
@@ -0,0 +1,61 @@
+<a href='/grandprix/anmeldung/'>Turnierarchiv/-anmeldung</a>
+
+
+    <!--  <br /><br /><b>08. bis 10. Juli</b><br />4. Peiner Schach-Kongress-->
+    <!-- <br /><a href='https://nsv-online.de/grandprix/ausschreibungen/peiner-schach-kongress-2022.pdf'>Ausschreibung</a>-->
+      <!--      | <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>-->
+    <!--     | <a href='https://nsv-online.de/turniere/peiner-schach-kongress-2022/?ak=peine2022&show=TeilRang#menu'>Ergebnisse</a>-->
+
+
+      <!--<br /><br /><b>03. bis 11. August</b><br />L&uuml;neburger Schachfestival-->
+     <!--     <br /><a href='http://nsv-grandprix.de/ausschreibungen/lueneburger-schachfestival-2019.pdf'>Ausschreibung</a>-->
+     <!--     | <a href='https://www.schachturnier-lueneburg.de/anmeldung/ '>Anmeldung</a>-->
+     <!--      | <a href='https://www.schachturnier-lueneburg.de/'>Ergebnisse</a>-->
+     <!--     <img src='http://nsv-online.de/grandprix/ausschreibungen/luneburg-logo.jpg' alt='Logo'>-->
+
+
+  <br /><br /><b>10. bis 13. August</b><br />Kurt Pape-Open
+  <br /><a href='/grandprix/ausschreibungen/KurtPapeOpen-2023.pdf'>Ausschreibung</a>
+  | <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>
+  <!--| <a href='https://nsv-online.de/2022/08/kyrill-scheck-gewinnt-1-kurt-pape-open-in-hameln/'>Ergebnisse</a>-->
+
+
+<!--  <br /><br /><b>19. bis 21. August</b><br />Lessing-Open in Wolfenb&uuml;ttel-->
+<!--  <br /><a href='/grandprix/ausschreibungen/lessing-open-2022.pdf'>Ausschreibung</a>-->
+  <!--| <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>-->
+<!--   | <a href='https://nsv-online.de/2022/08/nsv-grandprix-lessing-open-in-wolfenbuettel-hannes-ewert-gewinnt-a-turnier/'>Ergebnisse</a>-->
+
+
+<!-- <br /><br /><b>01. bis 03. Oktober</b><br />Tostedter Schachherbst-->
+<!-- <br /><a href='/grandprix/ausschreibungen/tostedter-schachherbst-2022.pdf'>Ausschreibung</a>-->
+  <!--| <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>-->
+<!--      | <a href='https://nsv-online.de/2022/10/tostedter-schachherbst-ergebnisse-gruppe-a-c/'>Ergebnisse</a>-->
+
+
+
+      <!--<br /><br /><b>24. bis 26. September</b><br />Ems-Vechte-Cup in Lingen -->
+       <!--   <br /><a href='http://nsv-grandprix.de/ausschreibungen/ems-vechte-cup-2021.pdf'>Ausschreibung</a> -->
+          <!--| <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>-->
+     <!--      | <a href='https://nsv-online.de/turniere/ems-vechte-cup-2021/?ak=EVC2021#menu'>Ergebnisse</a>-->
+           <!--| Ergebnisse <a href='http://ems-vechte-cup.de/?page_id=1325'>A</a>|<a href='http://ems-vechte-cup.de/?page_id=1320'>B</a>-->
+
+       <br /><br /><b>28. bis 31. Oktober</b><br />Schach-Open in Leer
+       <br /><a href='https://www.spass-mit-sport.de/veranstaltungen/6-schach-open-leer/'>Ausschreibung</a>
+   | <a href='https://www.spass-mit-sport.de/veranstaltungen/6-schach-open-leer/anmelden/'>Anmeldung</a>
+           | <a href='http://schach-open-leer.de/'>Turnierseite</a>
+       <!--          | <a href='https://nsv-online.de/2022/11/schach-open-in-leer-im-julian-kramer-sowie-mika-dorendorf-gewinnen/'>Ergebnisse</a>-->
+
+
+
+<!-- <br /><br /><b>02. bis 04. Dezember</b><br />Wieste-Cup in Sottrum-->
+<!-- <br /><a href='/grandprix/ausschreibungen/wieste-cup-2022.pdf'>Ausschreibung</a>-->
+<!--  | <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>-->
+     <!-- | <a href='https://nsv-online.de/turniere/wieste-cup-2021/?ak=Wieste-Cup&show=TeilRang&r=5#menu'>Ergebnisse</a>-->
+
+
+
+        <!--<br /><br /><b>15. bis 17. November</b><br />Andreas-Schaar-Gedenkturnier<br />in Nordhorn
+        <br /><a href='http://nsv-grandprix.de/ausschreibungen/Andreas-Schaar-Gedenkturnier-2019.pdf'>Ausschreibung</a>
+              | <a href='http://nsv-grandprix.de/anmeldung/'>Anmeldung</a>
+             | <a href='http://nsv-online.de/turniere/andreas-schaar-gedenkturnier-2019/?ak=Nordhorn%20Gruppe%20A#menu'>Ergebnisse</a>
+   -->
---
+++ nsv-online/mainmenu.menu.php
@@ -0,0 +1,78 @@
+<?
+$nsvMainmenu = array (
+
+"NSV" => array (
+	"nsv/",
+	(bool) strstr ( $_SERVER ['PHP_SELF'], "/archiv/" ),
+
+	"Neuigkeiten" => "",
+	"Verbandsnachrichten" => "verbandsnachrichten/",
+	"Vorstand" => "vorstand/",
+	"Kongress" => "kongress/",
+	"Satzung und Ordnungen" => "ordnungen/",
+	"Mitgliederverwaltung" => "mitgliederverwaltung/",
+
+	"Galerie der Meister" => "ruhmeshalle/",
+	"Ehrentafel" => "ehrentafel/",
+	"Vorstandschronik" => "vorstandschronik/"
+),
+
+"Ligen" => array (
+	"goto/nsvligen",
+	(bool) strstr ( $_SERVER ['PHP_SELF'], "/verband/Ligenuebersicht.php" ) ||  (bool) strstr ( $_SERVER ['PHP_SELF'], "/ligen/" ),
+	"&Uuml;bersicht" => "alle-ligen/",
+	"Landesliga Nord" => "goto/lln",
+	"Landesliga S&uuml;d" => "goto/lls",
+	"Verbandsliga Nord" => "goto/vln",
+	"Verbandsliga West" => "goto/vlw",
+	"Verbandsliga Ost" => "goto/vlo",
+	"Verbandsliga S&uuml;d" => "goto/vls",
+
+	"Pokal-Meisterschaft" => "ligen/pokal/",
+	"Frauen Landesliga" => "ligen/fll/",
+	"Jugendligen" => "ligen/nsj/"
+),
+
+"Vorstand" => array (
+	"vorstand/",
+	(bool) strstr ( $_SERVER ['REQUEST_URI'], "/vorstand/" )
+),
+
+"Bezirke" => array (
+	"bezirke/bezirk.php?kurzname=",
+	(bool) strstr ( $_SERVER ['PHP_SELF'], "/bezirke/" ),
+	"Hannover" => "bezirke/Hannover",
+	"Braunschweig" => "bezirke/Braunschweig",
+	"S&uuml;dniedersachsen" => "bezirke/Sued",
+	"L&uuml;neburg" => "bezirke/Lueneburg",
+	"Oldenburg-Ostfriesland" => "bezirke/Oldenburg",
+	"Osnabr&uuml;ck-Emsland" => "bezirke/Osnabrueck"
+),
+
+"Termine" => array (
+	"termine/",
+	(bool) strstr ( $_SERVER ['PHP_SELF'], "/termine/" )
+),
+
+"Ausbildung" => array (
+	"kategorie/ausbildung/",
+	false
+),
+
+"Frauen" => array (
+	"kategorie/frauenschach/",
+	false
+),
+
+"Jugend" => array (
+	"goto/jugend",
+	false
+),
+
+"Senioren" => array (
+	"goto/Senioren",
+	false
+)
+);
+?>
+
