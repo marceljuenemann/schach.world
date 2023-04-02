1521c3450e924741a220b254006ccfe301d377c4
Marcel Juenemann <git@marcel.world>
2023-04-02 12:41:45 +0100

    Exclude navigation-sidebar from local theme

---
+++ public/wordpress/wp-content/themes/nsv2020-local/sidebar/navigation-sidebar.php
@@ -0,0 +1,7 @@
+<div class='card shadow nsv-card nsv-sidebar-card'>
+  <div class='card-body'>
+    <h5 class='card-title'>Hello World!</h5>
+    This "navigation sidebar" is not currently included in the theme for local development.
+    It's nothing fancy though, just outputting some static links really.
+  </div>
+</div>
