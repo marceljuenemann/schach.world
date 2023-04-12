c45498deab6d407d6abcb947863b5f9e701857b1
Marcel Juenemann <git@marcel.world>
2023-04-12 14:00:37 +0100

    Move to standard symfony setup generated by CLI

--- public/wordpress/wp-content/themes/nsv2020/symfony.php
+++ public/wordpress/wp-content/themes/nsv2020/symfony.php
@@ -1,4 +1,29 @@
 <?php
+
+use Nsv\WebApp\Core\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+
+// TODO: move to static method?
+// TODO: Pass prod
+// TODO: Use Kernel Runner?
+$kernel = new Kernel('dev', true);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+
+
+
+/*
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+
+
+<?php
 // Handle dynamic routes with our own Symfony-based framework.
 
 use Nsv\WebApp\Kernel;
@@ -20,3 +45,4 @@ $kernel->terminate($request, $response);
 
 // TODO: handle 404
 // TODO: handle exceptions
+*/
---
+++ src/WebApp/Core/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nsv\WebApp\Core;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
