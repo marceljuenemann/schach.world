61c98bf316eedf231622be80cb9175bc0c32cea3
Marcel Juenemann <git@marcel.world>
2023-07-05 23:55:54 +0100

    Add nsv-v3 plugin for integrating Symfony

---
+++ public/wordpress/wp-content/plugins/nsv-v3/nsv-v3.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: NSV v3
+ * Plugin URI: https://nsv-online.de/
+ * Description: Core functionality turning nsv-online.de into an actual web app.
+ * Version: 1.0
+ * Author: Marcel Jünemann
+ * Author URI: http://marcel.world
+ */
+
+require_once(ABSPATH . '../../vendor/autoload.php');
+
+// Activate the Symfony based webapp? 
+add_filter('template_include', function($template) {
+  global $wp;
+  $prefixes = [
+    'v3',
+    '_error'
+  ];
+  foreach ($prefixes as $prefix) {
+    if (str_starts_with($wp->request, $prefix)) {
+      return locate_template('symfony.php');
+    }
+  }
+  return $template;
+});
