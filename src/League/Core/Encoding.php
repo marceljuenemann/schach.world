0e274d4f3a510d1cb96004806e0c153fb2283ab4
Marcel Juenemann <git@marcel.world>
2023-07-16 16:17:31 +0100

    Set up controller and service for Player view

--- src/League/Core/Encoding.php
+++ src/League/Core/Encoding.php
@@ -8,6 +8,7 @@ namespace Nsv\League\Core;
 class Encoding
 {
   // TODO: Move everything to Unicode
+  const CHARSET = 'ISO-8859-1';
   const UNICODE_ENABLED = false;
   const REMIS = '½';
 
@@ -22,14 +23,14 @@ class Encoding
    * Converts from UTF-8 to the application charset.
    */
   public static function utf8_decode($str) {
-    return mb_convert_encoding($str, 'ISO-8859-1', 'UTF-8');
+    return mb_convert_encoding($str, self::CHARSET, 'UTF-8');
   }
 
   /**
    * Converts from the application charset to UTF-8.
    */
   public static function utf8_encode($str) {
-    return mb_convert_encoding($str, 'UTF-8', 'ISO-8859-1');
+    return mb_convert_encoding($str, 'UTF-8', self::CHARSET);
   }
 
   /**
