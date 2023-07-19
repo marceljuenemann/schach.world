ae2b10ea332162bacbfbe47b07fd45253e741f53
Marcel Juenemann <git@marcel.world>
2023-07-19 22:25:09 +0200

    Mock DWZ calculation

--- tests/League/Api/Service/PlayerServiceTest.php
+++ tests/League/Api/Service/PlayerServiceTest.php
@@ -19,15 +19,11 @@ class PlayerServiceTest extends AbstractApiTest
 
   public function testPlayer1() {
     $player = $this->division->teams()[0]->players[0];
-/*
-    $newsRepository->expects(self::once())
-        ->method('findNewsFromLastMonth')
-        ->willReturn([
-            new News('some news'),
-            new News('some other news'),
-        ])
-    ;
-*/
+    $this->dwzService->expects(self::once())
+        ->method('calculate')
+        ->will($this->returnCallback(function() {
+          return func_get_args();
+        }));
 
     $model = $this->service->player($this->league, $player->id);
     $this->assertModel($model, __FILE__, __FUNCTION__);
@@ -35,12 +31,19 @@ class PlayerServiceTest extends AbstractApiTest
 
   public function testPlayer2() {
     $player = $this->division->teams()[1]->players[0];
+    $this->dwzService->expects(self::once())
+        ->method('calculate')
+        ->will($this->returnCallback(function() {
+          return func_get_args();
+        }));
+
     $model = $this->service->player($this->league, $player->id);
     $this->assertModel($model, __FILE__, __FUNCTION__);
   }
 
   public function testPlayer3_withoutGamesAndRating() {
     $player = $this->division->teams()[0]->players[1];
+    $this->dwzService->expects(self::never())->method('calculate');
     $model = $this->service->player($this->league, $player->id);
     $this->assertModel($model, __FILE__, __FUNCTION__);
   }
