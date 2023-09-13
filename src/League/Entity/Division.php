90c0edf2bc7b032c13606e0cac73a2eac72f8d5a
Marcel Juenemann <git@marcel.world>
2023-09-13 14:25:15 +0200

    Formatting fix

--- src/League/Entity/Division.php
+++ src/League/Entity/Division.php
@@ -14,152 +14,152 @@ use Nsv\Util\TextSanitizer;
 #[ORM\Table(name: 'staffeln')]
 class Division
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private int $id;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private int $id;
 
-    #[ORM\ManyToOne(targetEntity: League::class, inversedBy: 'divisions')]
-    #[ORM\JoinColumn(name: "turnier", referencedColumnName: "id")]
-    private $league;
+  #[ORM\ManyToOne(targetEntity: League::class, inversedBy: 'divisions')]
+  #[ORM\JoinColumn(name: "turnier", referencedColumnName: "id")]
+  private $league;
 
-    #[ORM\Column(length: 30)]
-    private string $name;
+  #[ORM\Column(length: 30)]
+  private string $name;
 
-    #[ORM\Column(name: "sortid")]
-    private ?int $sortId;
+  #[ORM\Column(name: "sortid")]
+  private ?int $sortId;
 
-    /**
-     * The legacy user of the league manager.
-     * 
-     * TODO: Use WordPress user system.
-     * TODO: Allow multiple users to manage a league.
-     */
-    #[ORM\OneToOne(targetEntity: LegacyUser::class)]
-    #[ORM\JoinColumn(name: 'leiter', referencedColumnName: 'id')]
-    private $manager;
+  /**
+   * The legacy user of the league manager.
+   * 
+   * TODO: Use WordPress user system.
+   * TODO: Allow multiple users to manage a league.
+   */
+  #[ORM\OneToOne(targetEntity: LegacyUser::class)]
+  #[ORM\JoinColumn(name: 'leiter', referencedColumnName: 'id')]
+  private $manager;
 
-    /**
-     * Number of rounds. If set to null, the league-wide config should be used.
-     */
-    #[ORM\Column(name: 'runden')]
-    private int|null $configRounds = null;
+  /**
+   * Number of rounds. If set to null, the league-wide config should be used.
+   */
+  #[ORM\Column(name: 'runden')]
+  private int|null $configRounds = null;
 
-    /**
-     * Whether to show player numbers in the UI. If set to null, the league-wide
-     * configuration should be used.
-     */
-    #[ORM\Column(name: 'showPassNr')]
-    private bool|null $configPlayerNumbers = null;
+  /**
+   * Whether to show player numbers in the UI. If set to null, the league-wide
+   * configuration should be used.
+   */
+  #[ORM\Column(name: 'showPassNr')]
+  private bool|null $configPlayerNumbers = null;
 
-    /**
-     * Whether to show late registrations on the match day page.
-     */
-    #[ORM\Column(name: 'showNachmeldungen')]
-    private bool|null $configShowLateRegistrations = null;
+  /**
+   * Whether to show late registrations on the match day page.
+   */
+  #[ORM\Column(name: 'showNachmeldungen')]
+  private bool|null $configShowLateRegistrations = null;
 
-    /**
-     * Whether to show a preview of the next match day.
-     */
-    #[ORM\Column(name: 'showSpieltagvorschau')]
-    private bool|null $configShowNextMatchDay = null;
+  /**
+   * Whether to show a preview of the next match day.
+   */
+  #[ORM\Column(name: 'showSpieltagvorschau')]
+  private bool|null $configShowNextMatchDay = null;
 
-    #[ORM\OneToMany(targetEntity: Pairing::class, mappedBy: 'division')]   
-    #[ORM\OrderBy(Pairing::ORDERING)]
-    private $pairings;
+  #[ORM\OneToMany(targetEntity: Pairing::class, mappedBy: 'division')]   
+  #[ORM\OrderBy(Pairing::ORDERING)]
+  private $pairings;
 
-    #[ORM\OneToMany(targetEntity: RoundComment::class, mappedBy: 'division')]   
-    private $roundComments;
+  #[ORM\OneToMany(targetEntity: RoundComment::class, mappedBy: 'division')]   
+  private $roundComments;
 
-    public function path() {
-      return TextSanitizer::slug($this->name);
+  public function path() {
+    return TextSanitizer::slug($this->name);
+  }
+
+  /**
+   * Returns all Date entities for this division, keyed by round number.
+   * 
+   * @return array round => Entity\Date.
+   */
+  public function dates(): array {
+    $dates = [];
+    foreach ($this->league->dates as $date) {
+      if ($date->division && $date->division != $this) continue;
+      if (isset($dates[$date->round])) continue;  // Dates are sorted by most specific first.
+      $dates[$date->round] = $date;
     }
+    uasort($dates, [Date::class, 'compare']);
+    return $dates;
+  }
 
-    /**
-     * Returns all Date entities for this division, keyed by round number.
-     * 
-     * @return array round => Entity\Date.
-     */
-    public function dates(): array {
-      $dates = [];
-      foreach ($this->league->dates as $date) {
-        if ($date->division && $date->division != $this) continue;
-        if (isset($dates[$date->round])) continue;  // Dates are sorted by most specific first.
-        $dates[$date->round] = $date;
-      }
-      uasort($dates, [Date::class, 'compare']);
-      return $dates;
-    }
+  /**
+   * Returns the Round object for the given round.
+   */
+  public function round(int $round): Round {
+    $dates = $this->dates();
+    $date = isset($dates[$round]) ? $dates[$round]->date : null;
+    return new Round($this, $round, $date);
+  }
 
-    /**
-     * Returns the Round object for the given round.
-     */
-    public function round(int $round): Round {
-      $dates = $this->dates();
-      $date = isset($dates[$round]) ? $dates[$round]->date : null;
-      return new Round($this, $round, $date);
-    }
-
-    /**
-     * Returns all Rounds for which a date has been set.
-     */
-    // TODO: Use Rounds instead of Dates everywhere possible.
-    public function roundsWithDate(): array {
-      $maxRound = $this->config('rounds');
-      $rounds = [];
-      foreach ($this->dates() as $date) {
-        if ($date->round <= $maxRound) {
-          $rounds[$date->round] = new Round($this, $date->round, $date->date);
-        }
-      }
-      return $rounds;
-    }
-
-    public function teams(): array {
-      $teams = [];
-      foreach ($this->league->teams as $team) {
-        if ($team->division == $this) {
-          $teams[] = $team;
-        }
-      }
-      return $teams;
-    }
-
-    /**
-     * Returns a config property. If the value is set to null on the division
-     * the setting on the League will be used.
-     */
-    public function config($key): mixed {
-      $key = 'config' . ucfirst($key);
-      $value = $this->$key;
-      if ($value === null) {
-        return $this->league->$key;
-      } else {
-        return $value;
+  /**
+   * Returns all Rounds for which a date has been set.
+   */
+  // TODO: Use Rounds instead of Dates everywhere possible.
+  public function roundsWithDate(): array {
+    $maxRound = $this->config('rounds');
+    $rounds = [];
+    foreach ($this->dates() as $date) {
+      if ($date->round <= $maxRound) {
+        $rounds[$date->round] = new Round($this, $date->round, $date->date);
       }
     }
+    return $rounds;
+  }
 
-    public function uri(): string {
-      return $this->league->uri() . "?staffel={$this->id}&r=";
+  public function teams(): array {
+    $teams = [];
+    foreach ($this->league->teams as $team) {
+      if ($team->division == $this) {
+        $teams[] = $team;
+      }
     }
+    return $teams;
+  }
 
-    public function scheduleUri() {
-      return $this->league->uri() . $this->path() . "/spielplan/";
+  /**
+   * Returns a config property. If the value is set to null on the division
+   * the setting on the League will be used.
+   */
+  public function config($key): mixed {
+    $key = 'config' . ucfirst($key);
+    $value = $this->$key;
+    if ($value === null) {
+      return $this->league->$key;
+    } else {
+      return $value;
     }
+  }
 
-    public function statsUri() {
-      return $this->league->uri() . "?staffel={$this->id}&r=statistik";
-    }
+  public function uri(): string {
+    return $this->league->uri() . "?staffel={$this->id}&r=";
+  }
 
-    public function __call($property, $args) {
-      return $this->$property;
-    }
+  public function scheduleUri() {
+    return $this->league->uri() . $this->path() . "/spielplan/";
+  }
 
-    public function __get($property) {
-      return $this->$property;
-    }
+  public function statsUri() {
+    return $this->league->uri() . "?staffel={$this->id}&r=statistik";
+  }
 
-    public function __set($property, $value) {
-      $this->$property = $value;
-    }
+  public function __call($property, $args) {
+    return $this->$property;
+  }
+
+  public function __get($property) {
+    return $this->$property;
+  }
+
+  public function __set($property, $value) {
+    $this->$property = $value;
+  }
 }
