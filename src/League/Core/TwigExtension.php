6e5bff173c6685a21b40eae24f6dbc6c6ac6fa53
Marcel Juenemann <git@marcel.world>
2023-09-14 17:55:25 +0200

    New league navigation
    Less clutered navigation that integrations season and division selection

--- src/League/Core/TwigExtension.php
+++ src/League/Core/TwigExtension.php
@@ -9,7 +9,9 @@ class TwigExtension extends AbstractExtension {
 
   function getFunctions(): array {
     return [
-      TwigFunctions::outputBuffering('legacy_league_header', function($title) {
+      TwigFunctions::outputBuffering('legacy_league_header', function($title, $isHomescreen) {
+        global $globals;
+        $globals['isHomescreen'] = $isHomescreen;  // hint for navigation bar.
         include_once('gui.inc.php');
       }),
 
