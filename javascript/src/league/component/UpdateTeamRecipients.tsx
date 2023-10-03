d767b7109cc5a6d72c3ba8c49325365ddf023aa2
Marcel Juenemann <git@marcel.world>
2023-10-03 18:02:12 +0200

    EmailList component for additional recipients dialog

--- javascript/src/league/component/UpdateTeamRecipients.tsx
+++ javascript/src/league/component/UpdateTeamRecipients.tsx
@@ -1,13 +1,61 @@
 import { NsvFormProps } from "../../core/form";
-import React, { ReactNode } from "react";
+import React, { FormEvent, ReactNode } from "react";
 import { NsvLoadingDialog } from "../../core/dialog";
+import { Button, Card, Form, InputGroup } from "react-bootstrap";
 
 /**
  * Shows a list of email addresses, allowing to add and delete them. 
  */
-class EmailList extends React.Component<{emails: Array<String>}, {emails: Array<string>}> {
+class EmailList extends React.Component<{emails: Array<String>}, {emails: Array<string>, newEmail: string}> {
 
+  constructor(props: {emails: Array<string>}) {
+    super(props)
+    this.state = {emails: props.emails, newEmail: ''}
+  }
 
+  onSubmit(e: FormEvent) {
+    this.setState({
+      emails: [...this.state.emails, this.state.newEmail],
+      newEmail: ''
+    })
+    e.preventDefault()
+  }
+
+  remove(index: number) {
+    const list = this.state.emails
+    list.splice(index, 1)
+    this.setState({emails: list})
+  }
+    
+  render() {
+    return <Form onSubmit={this.onSubmit.bind(this)}>
+      <InputGroup>
+        <InputGroup.Text>
+          <span className="dashicons dashicons-plus"></span>
+        </InputGroup.Text>
+        <Form.Control
+          placeholder="E-Mail Empfänger hinzufügen"
+          type="email"
+          required={true}
+          value={this.state.newEmail}
+          onChange={e => this.setState({newEmail: e.target.value})}
+        />
+        <Button type="submit" variant="primary" id="button-addon2">
+          Hinzufügen
+        </Button>        
+      </InputGroup>
+      {
+        this.state.emails.map((email, index) => {
+          return <Card className="mt-2" key={email}>
+            <Card.Body className="d-flex justify-content-between">
+              <span>{ email }</span>
+              <span onClick={() => this.remove(index)} className="dashicons dashicons-trash" style={{cursor: "pointer"}}></span>
+            </Card.Body>
+          </Card>
+        })
+      }
+     </Form>
+  }
 }
 
 export class UpdateTeamRecipientsDialog extends NsvLoadingDialog<{recipients: Array<String>}, {teamId: number}> {
@@ -19,6 +67,6 @@ export class UpdateTeamRecipientsDialog extends NsvLoadingDialog<{recipients: Ar
   }
 
   renderBodyWithProps(props: {recipients: Array<string>} & NsvFormProps): ReactNode {
-    return <div {...this.props} {...props}>Hello World</div>
+    return <EmailList emails={props.recipients} />
   }
 }
