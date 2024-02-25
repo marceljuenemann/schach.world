252ab3792ac96ef250d5539afffd6799b85714ff
Marcel Juenemann <mail@marcel-juenemann.de>
2024-02-25 23:42:35 +0000

    Club rewrite: Show OpenStreetMap with react-leaflet

--- javascript/src/webapp/component/ClubsMap.tsx
+++ javascript/src/webapp/component/ClubsMap.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+// TODO: Determine automatically
+const INIT_LAT = 52.8861352
+const INIT_LON = 9.1848507
+const INIT_ZOOM = 7
 
 /**
  * Displays a map with clubs.
@@ -15,9 +21,18 @@ export class ClubsMap extends React.Component<{
 
   render() {
     return (
-      <div>
-        Hi React!
-      </div>
+      <MapContainer style={{height: '100%'}} center={[INIT_LAT, INIT_LON]} zoom={INIT_ZOOM} scrollWheelZoom={true}>
+        {/* TODO: Attribute schach.in */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[INIT_LAT, INIT_LON]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     );
   }
 }
