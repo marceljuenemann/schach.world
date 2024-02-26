7f0885cfb61e1949565435c0a4b60d9fa30642ad
Marcel Juenemann <mail@marcel-juenemann.de>
2024-02-26 23:44:59 +0000

    Club rewrite: Fetch geojson and cache data generation

--- src/WebApp/Controller/ClubController.php
+++ src/WebApp/Controller/ClubController.php
@@ -9,10 +9,15 @@ use Nsv\Util\TextSanitizer;
 use Nsv\WebApp\Core\ApiResponse;
 use SimpleXMLElement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
+const CACHE_EXPIRATION = 60 * 60 * 24;  // 1 day
 const SCHACH_IN_URL = 'https://schach.in/';
+const SCHACH_IN_GEOJSON = SCHACH_IN_URL . 'niedersachsen.geojson';
 
 // Schach.in does not provide ZPS in the export, so we try to match by name...
 const SCHACH_IN_HACKS = [
@@ -23,15 +28,18 @@ const SCHACH_IN_HACKS = [
 #[Route('/vereine/', name: 'club_')]
 class ClubController extends AbstractController {
 
+  private CacheInterface $cache;
+
   function __construct(
     private EntityManagerInterface $leagueEntityManager,
     private string $projectDir
-  ) {}
+  ) {
+    $this->cache = new FilesystemAdapter('nsvclub');
+  }
 
   #[Route('', name: 'index')]
   public function clubs(): Response {
-    // TODO: Add cache
-    $districts = $this->fetchDistricts();
+    $districts = $this->fetchDistrictsCached();
     return $this->render('club/clubs.html.twig', [
       'districts' => $districts,
       'jsonData' => json_encode($districts)
@@ -42,6 +50,16 @@ class ClubController extends AbstractController {
   public function clubs_api(): Response {
     $data = $this->fetchDistricts();
     return new ApiResponse($data);
+  }
+
+  /**
+   * Cached version of fetchDistricts().
+   */
+  private function fetchDistrictsCached(): array {
+    return $this->cache->get('districts', function (ItemInterface $item): array {
+      $item->expiresAfter(CACHE_EXPIRATION);
+      return $this->fetchDistricts();
+    });
   }
 
   /**
@@ -91,8 +109,7 @@ class ClubController extends AbstractController {
    * Fetches schach.in map data.
    */
   private function fetchMapData() {
-    // TODO: Load from prod.
-    $content = file_get_contents(dirname(__FILE__) . '/tmp.json');
+    $content = file_get_contents(SCHACH_IN_GEOJSON);
     foreach (json_decode($content)->features as $feature) {
       // TODO: Ask schach.in to provide ZPS to avoid matching by name.
       $slug = TextSanitizer::slug($feature->properties->org);
