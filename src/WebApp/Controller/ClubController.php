a4d23aa856fd0e4d7d6b2c88ce0444e18e2390af
Marcel Juenemann <mail@marcel-juenemann.de>
2024-02-26 00:34:04 +0000

    Club rewrite: parse schach.in data

--- src/WebApp/Controller/ClubController.php
+++ src/WebApp/Controller/ClubController.php
@@ -5,10 +5,17 @@ namespace Nsv\WebApp\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Nsv\Dwz\DsbDatabase;
 use Nsv\League\Core\Encoding;
+use Nsv\Util\TextSanitizer;
 use Nsv\WebApp\Core\ApiResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+// Schach.in does not provide ZPS in the export, so we try to match by name...
+const SCHACH_IN_HACKS = [
+  'mtv-salzhausen' => 'sabt-mtv-salzhausen',
+  'sc-schwarzer-springer-bad-zwischenahn' => 'sc-schw-springer-bad-zwischenahn' 
+];
 
 #[Route('/vereine/', name: 'club_')]
 class ClubController extends AbstractController {
@@ -20,6 +27,7 @@ class ClubController extends AbstractController {
 
   #[Route('', name: 'index')]
   public function clubs(): Response {
+    // TODO: Add cache
     $districts = $this->fetchDistricts();
     return $this->render('club/clubs.html.twig', ['districts' => $districts]);
   }
@@ -27,6 +35,7 @@ class ClubController extends AbstractController {
   #[Route('api/', name: 'api')]
   public function clubs_api(): Response {
     $data = $this->fetchDistricts();
+    //$data = $this->fetchMapData();
     return new ApiResponse($data);
   }
 
@@ -36,7 +45,12 @@ class ClubController extends AbstractController {
   private function fetchDistricts(): array {
     $districts = require($this->projectDir . '/public/core/nsv2020/bezirke.php');
     $clubs = $this->fetchClubs();
+    $mapData = $this->fetchMapData();
     foreach ($clubs as $club) {
+      $slug = TextSanitizer::slug($club['name']);
+      if (isset($mapData[$slug])) {
+        $club['properties'] = $mapData[$slug];
+      }
       $districts[$club['district']]['clubs'][$club['zps']] = $club;
     }
     return $districts;
@@ -61,5 +75,23 @@ class ClubController extends AbstractController {
         'dwzUri' => DsbDatabase::clubUri($club['ZPS'])
       ];
     }, $clubs);
+  }
+
+  /**
+   * Fetches schach.in map data.
+   */
+  private function fetchMapData() {
+    // TODO: Load from prod.
+    $content = file_get_contents(dirname(__FILE__) . '/tmp.json');
+    foreach (json_decode($content)->features as $feature) {
+      $feature->properties->coordinates = $feature->geometry->coordinates;
+      // TODO: Ask schach.in to provide ZPS to avoid matching by name.
+      $slug = TextSanitizer::slug($feature->properties->org);
+      $clubs[$slug] = $feature->properties;
+    }
+    foreach (SCHACH_IN_HACKS as $from => $to) {
+      $clubs[$to] = $clubs[$from];
+    }
+    return $clubs;
   }
 }
