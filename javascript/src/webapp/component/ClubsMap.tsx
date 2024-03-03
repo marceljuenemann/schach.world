05670673999d3871013f388e1b1cadb043531e41
Marcel Juenemann <mail@marcel-juenemann.de>
2024-03-03 20:31:47 +0000

    Bezirk Hannover: adjust clubs page for single district

--- javascript/src/webapp/component/ClubsMap.tsx
+++ javascript/src/webapp/component/ClubsMap.tsx
@@ -2,11 +2,6 @@ import L, { Icon, LatLngTuple } from 'leaflet';
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
-// TODO: Determine automatically
-const INIT_LAT = 52.77
-const INIT_LON = 9.07
-const INIT_ZOOM = 7
-
 export interface District {
   name: string
   website: string
@@ -38,8 +33,12 @@ export interface Club {
  * Displays a map with clubs.
  */
 export class ClubsMap extends React.Component<{
-    districts: District[]
-  }, {
+    data: {
+      districts: District[],
+      lat: number,
+      lon: number,
+      zoom: number
+    }
   }> {
 
   constructor(props: any) {
@@ -48,26 +47,28 @@ export class ClubsMap extends React.Component<{
 
   render() {
     L.Icon.Default.imagePath = '/core/nsv2020/images/marker/';
+    const coordinates = [this.props.data.lat, this.props.data.lon] as LatLngTuple
+    const showDistrict = this.props.data.districts.length > 1
     return (
-      <MapContainer style={{height: '100%'}} center={[INIT_LAT, INIT_LON]} zoom={INIT_ZOOM} scrollWheelZoom={true}>
+      <MapContainer style={{height: '100%'}} center={coordinates} zoom={this.props.data.zoom} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a href="https://schach.in">schach.in</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {
-          Object.values(this.props.districts).map(district => {
+          Object.values(this.props.data.districts).map(district => {
             const icon = new L.Icon.Default({
               iconUrl: `marker-icon-${district.markerColor}.png`,
               iconRetinaUrl: `marker-icon-${district.markerColor}-2x.png`
             }) as Icon;
-            return Object.values(district.clubs).map(club => this.renderClub(club, district, icon))
+            return Object.values(district.clubs).map(club => this.renderClub(club, district, showDistrict, icon))
           }).flat()
         }
       </MapContainer>
     );
   }
 
-  private renderClub(club: Club, district: District, icon: Icon) {
+  private renderClub(club: Club, district: District, showDistrict: boolean, icon: Icon) {
     const coordinates = [parseFloat(club.venue!.latitude), parseFloat(club.venue!.longitude)] as [number, number]
     return (
       <Marker key={club.zps} position={coordinates} icon={icon}>
@@ -81,7 +82,7 @@ export class ClubsMap extends React.Component<{
 
           {club.stats.members} Mitglieder ({club.stats.members_u25} U25)<br />
           ø-DWZ: {club.stats.avg_rating}&nbsp;&nbsp;ø-Alter: {club.stats.avg_age}<br />
-          <a href={district.website}>{district.name}</a>
+          {showDistrict && <a href={district.website}>{district.name}</a>}
         </Popup>
       </Marker>
     )
