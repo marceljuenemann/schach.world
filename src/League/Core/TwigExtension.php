18b61265dc292658ea0be4c9d01e4de01c060b2b
Thomas Moseler <tmoseler@gmx.de>
2024-03-13 12:04:16 +0100

    Removed all but one usage of raw filter from table macro. Tried to create a Twig filter that unescapes html entities. Does not quite work...

--- src/League/Core/TwigExtension.php
+++ src/League/Core/TwigExtension.php
@@ -2,10 +2,17 @@
 
 namespace Nsv\League\Core;
 
+use Nsv\League\Core\Encoding;
 use Nsv\Util\TwigFunctions;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 
 class TwigExtension extends AbstractExtension {
+
+  public function __construct(private Encoding $encoding) {
+
+  }
+
 
   function getFunctions(): array {
     return [
@@ -17,5 +24,17 @@ class TwigExtension extends AbstractExtension {
 
       TwigFunctions::outputBuffering('legacy_league_footer', 'SED_GUIclose'),
     ];
+  }
+
+  public function getFilters()
+  {
+    return array(
+      new TwigFilter('unescape_html_entity', [$this, 'unescape_html_entity']),
+    );
+  }
+
+  public function unescape_html_entity($value)
+  {
+    return $this->encoding->utf8_decode(html_entity_decode($value));
   }
 }
