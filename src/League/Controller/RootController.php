32fac4159b4314667b4ed6fee9d0319b858e38ae
Marcel Juenemann <mail@marcel-juenemann.de>
2024-03-16 10:55:17 +0000

    TinyURLs: Redirect /ligen/3/

--- src/League/Controller/RootController.php
+++ src/League/Controller/RootController.php
@@ -2,6 +2,7 @@
 
 namespace Nsv\League\Controller;
 
+use Nsv\League\Repository\DivisionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,19 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/ligen/', name: 'league_root_')]
 class RootController extends AbstractController {
 
+  function __construct(
+    private DivisionRepository $divisionRepository
+  ) {}
+
   #[Route('', name: 'root')]
   public function root(): Response {
     // Redirect to an overview page outside the league manager.
     return $this->redirect('/alle-ligen/');
+  }
+
+  #[Route('3/{divisionId}', name: 'division_login')]
+  public function divisionLogin(int $divisionId): Response {
+    $division = $this->divisionRepository->find($divisionId);
+    return $this->redirect($division->league->uri() . '?m=startseite&staffel=' . $division->id);
   }
 }
