ea2151f60ec3ecace3d6b5130f408078ca69363a
Thomas Moseler <tmoseler@gmx.de>
2024-04-02 16:34:28 +0200

    Add a service to calculate the ranking

---
+++ src/League/Api/Service/RankingService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nsv\League\Api\Service;
+
+class RankingService {
+
+  public function __construct (private EntityManagerInterface $leagueEntityManager) {
+
+  }
+
+  /**
+   * A temporary method to get started
+   */
+  public function rankingTemp() {
+
+  }
+}
