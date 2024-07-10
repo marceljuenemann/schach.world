5f5eec7d8069b088c0fa22c2332684d9420b530e
Marcel Juenemann <mail@marcel-juenemann.de>
2024-07-10 23:17:57 +0100

    DWZ: Create full Player and Club entities

--- src/Dwz/Controller/DwzController.php
+++ src/Dwz/Controller/DwzController.php
@@ -7,6 +7,7 @@ use Nsv\Dwz\Entity\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Controller for the DWZ API.
@@ -14,9 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/dwz/api/', name: 'dwz_')]
 class DwzController extends AbstractController {
 
+  function __construct(private SerializerInterface $serializer) {}
+
   #[Route('players/', name: 'players')]
   public function players(EntityManagerInterface $mainEntityManager): Response {
-    $player = $mainEntityManager->getRepository(Player::class)->find('Jünemann,Marcel');
-    return new Response($player->name);
+    $player = $mainEntityManager->getRepository(Player::class)->findByName('Jünemann,Marcel');
+    $json = $this->serializer->serialize($player, 'json');
+    return new Response($json);
   }
 }
