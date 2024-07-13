ced8f3b2d2f6b39019fe4ffd708c315f2e9d170f
Marcel Juenemann <mail@marcel-juenemann.de>
2024-07-13 12:48:31 +0100

    DWZ: Add API for club search

--- src/Dwz/Controller/DwzController.php
+++ src/Dwz/Controller/DwzController.php
@@ -3,10 +3,12 @@
 namespace Nsv\Dwz\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Nsv\Dwz\Entity\Club;
 use Nsv\Dwz\Entity\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -16,12 +18,30 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 #[Route('/dwz/api/', name: 'dwz_')]
 class DwzController extends AbstractController {
 
-  function __construct(private NormalizerInterface $normalizer) {}
+  function __construct(
+    private EntityManagerInterface $mainEntityManager,
+    private NormalizerInterface $normalizer
+  ) {}
 
   #[Route('players/', name: 'players')]
-  public function players(EntityManagerInterface $mainEntityManager): Response {
-    $player = $mainEntityManager->getRepository(Player::class)->findOneByName('Jünemann,Marcel');
+  public function players(): Response {
+    $player = $this->mainEntityManager->getRepository(Player::class)->findOneByName('Jünemann,Marcel');
     $data = $this->normalizer->normalize($player);
     return new JsonResponse($data);
+  }
+
+  #[Route('clubs/', name: 'clubs')]
+  public function clubs(#[MapQueryParameter] string $name, #[MapQueryParameter] string $zps): Response {
+    $entities = $this->mainEntityManager->getRepository(Club::class)
+      ->createQueryBuilder('club')
+      ->where('club.name LIKE :name')
+      ->andWhere('club.zps LIKE :zps')
+      ->orderBy('club.name', 'ASC')
+      ->setMaxResults(10)
+      ->setParameter('name', "%$name%")
+      ->setParameter('zps', "$zps%")
+      ->getQuery()
+      ->getResult();
+    return new JsonResponse($this->normalizer->normalize($entities));
   }
 }
