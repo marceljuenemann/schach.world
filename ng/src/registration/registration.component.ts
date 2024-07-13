f91d037c946eb732f37376aadbb0eee2e29765dc
Marcel Juenemann <mail@marcel-juenemann.de>
2024-07-13 18:08:53 +0100

    Registraion: launch dialog

--- ng/src/registration/registration.component.ts
+++ ng/src/registration/registration.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'nsv-registration',
   standalone: true,
-  imports: [NgbAlertModule],
+  imports: [],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
 
+  constructor(private modalService: NgbModal) {}
+
+  openRegistration(content: TemplateRef<any>) {
+    this.modalService.open(content).result.then(
+      (result) => {
+        console.log(`Closed with: ${result}`)
+      }
+    )
+  }
 }
