7d86f10dbfc256fe3a5ceffece2b5924bc7305ad
Marcel Juenemann <mail@marcel-juenemann.de>
2024-07-14 16:53:01 +0100

    Registraion: Set up FormGroup

--- ng/src/registration/player-data/player-data.component.ts
+++ ng/src/registration/player-data/player-data.component.ts
@@ -1,22 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { model, Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { PlayerData, DwzService } from '../../dwz/dwz.service';
 import { debounceTime, Observable, of, switchMap } from 'rxjs';
 import { NgbTypeahead, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'player-data',
   standalone: true,
-  imports: [FormsModule, NgbTypeaheadModule, JsonPipe],
+  imports: [FormsModule, ReactiveFormsModule, NgbTypeaheadModule, JsonPipe],
   templateUrl: './player-data.component.html',
   styleUrl: './player-data.component.css'
 })
-export class PlayerDataComponent {
-	player: PlayerData|undefined
+export class PlayerDataComponent implements OnInit {
+  // The selected database entry, or the player name in case of manual input.
+	// selectedPlayer = model<PlayerData|string>('')
+
+  form = new FormGroup({
+    selectedPlayer: new FormControl<PlayerData|string>(''),
+    club: new FormControl(''),
+    zps: new FormControl(''),
+    memberId: new FormControl(''),
+    yearOfBirth: new FormControl(''),
+    gender: new FormControl(''),
+    dwz: new FormControl(''),
+    elo: new FormControl(''),
+    fideId: new FormControl(''),
+    fideTitle: new FormControl(''),
+  });
+
   @Output() playerSelected = new EventEmitter<PlayerData|undefined>();
 
   constructor(private dwz: DwzService) {}
+
+  ngOnInit() {
+    this.form.controls.selectedPlayer.valueChanges.subscribe(player => {
+      if (player && typeof player === 'object') {
+        this.form.controls.club.setValue(player.club)
+        this.form.controls.zps.setValue(player.zps)
+        this.form.controls.memberId.setValue(player.memberId)
+      }
+    })
+  }
+
 
 	search = (text$: Observable<string>) => {
 		return text$.pipe(
