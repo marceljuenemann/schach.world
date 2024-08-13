0df1369d834b50492ff18771a3926da58fe44d56
Thomas Moseler <tmoseler@gmx.de>
2024-08-13 11:50:35 +0200

    Removing web profiler bundle again

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
