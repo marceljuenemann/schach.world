ecc5f77170454fd43a551a8df0d89ed3531ca484
Thomas Moseler <tmoseler@gmx.de>
2024-10-04 18:08:39 +0200

    New Twig filter so we can format .5 as 1/2 in results directly in Twig. Used in template for new ranking.

--- src/League/Core/TwigExtension.php
+++ src/League/Core/TwigExtension.php
@@ -2,8 +2,11 @@
 
 namespace Nsv\League\Core;
 
+use Nsv\League\Core\Encoding;
+use Nsv\League\Core\Result;
 use Nsv\Util\TwigFunctions;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 
 class TwigExtension extends AbstractExtension {
 
@@ -17,5 +20,27 @@ class TwigExtension extends AbstractExtension {
 
       TwigFunctions::outputBuffering('legacy_league_footer', 'SED_GUIclose'),
     ];
+  }
+  public function getFilters(): array {
+    return [
+      new TwigFilter('format_float_result', [$this, 'format_float_result']),
+    ];
+  }
+
+  public function halfPoint() {
+    return Encoding::utf8_decode(Result::UNICODE_DRAW);
+  }
+
+  /**
+   * Format floats as strings. Replace .5 with '½'
+   */
+  public function format_float_result($float) {
+    $floatToString = strval($float);
+    if($floatToString == '0.5') {
+      $formattedResult = $this->halfPoint();
+    } else {
+      $formattedResult = str_replace('.5', $this->halfPoint(), $floatToString);
+    }
+    return $formattedResult;
   }
 }
