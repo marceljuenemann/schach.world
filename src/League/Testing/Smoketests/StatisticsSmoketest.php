9d779768d03fe5d83afce5672a2811485f3e0007
Thomas Moseler <tmoseler@gmx.de>
2025-05-19 15:31:25 +0200

    Now our service is detected as a service finally.

--- src/League/Testing/Smoketests/StatisticsSmoketest.php
+++ src/League/Testing/Smoketests/StatisticsSmoketest.php
@@ -3,20 +3,26 @@
 namespace Nsv\League\Testing\Smoketests;
 
 use Nsv\Util\Testing\Smoketest\SmoketestInferface;
+use Nsv\Util\Testing\Smoketest\SmoketestProvider;
 
-class StatisticsSmoketest implements SmoketestInferface {
+class StatisticsSmoketest {
 
-    /**
-     * @inheritDoc
-     */
-    public function urls(): array {
-        // TODO: Implement urls() method.
-    }
+  /**
+   * @inheritDoc
+   */
+  public function urls(): array {
+    // TODO: Implement urls() method.
+  }
 
-    /**
-     * @inheritDoc
-     */
-    public function transport(): string {
-        // TODO: Implement transport() method.
-    }
+  /**
+   * @inheritDoc
+   */
+  public function transport(): string {
+    // TODO: Implement transport() method.
+  }
+
+  public function execute() {
+    // TODO: Implement execute() method.
+    return ('I would be a smoketest');
+  }
 }
