f776f1c56df730adf16541e81efed4afe41753a6
Thomas Moseler <tmoseler@gmx.de>
2025-05-22 14:35:29 +0200

    Starting to build the logic for testing URLs

--- src/Util/Testing/Smoketest/SmoketestProvider.php
+++ src/Util/Testing/Smoketest/SmoketestProvider.php
@@ -1,24 +1,39 @@
 <?php
 
-namespace  Nsv\Util\Testing\Smoketest;
+namespace Nsv\Util\Testing\Smoketest;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Runs smoketests against a given array of URLs.
  */
 class SmoketestProvider implements SmoketestInterface {
 
+
   public function __construct(protected LoggerInterface $logger) {}
 
   public function urls(): array {
-    // TODO: Implement urls() method.
   }
 
   public function transport(): string {
     // TODO: Implement transport() method.
   }
 
-  public function execute() {
+  public function execute() {}
+
+  protected function checkUrls(): array {
+    $urls = $this->urls();
+    $responses = [];
+    foreach ($urls as $url) {
+      $responses[] = $this->requestUrl($url);
+    }
+    return $responses;
+  }
+
+  protected function requestUrl(array $urls): array {
+    $response = [];
+    return $response;
   }
 }
