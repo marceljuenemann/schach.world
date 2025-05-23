eb7176e0528647228a822b0890fae11eb5e63d9c
Thomas Moseler <tmoseler@gmx.de>
2025-05-23 12:34:42 +0200

    Instead of using httpclient using direct cUrl

--- src/Util/Testing/Smoketest/SmoketestProvider.php
+++ src/Util/Testing/Smoketest/SmoketestProvider.php
@@ -31,18 +31,48 @@ class SmoketestProvider implements SmoketestInterface {
   protected function checkUrls(): array {
     $baseUrl = $this->getBaseURL();
     $urls = $this->urls();
+    $cSession = $this->startCurlSession();
     $responses = [];
     foreach ($urls as $url) {
-      $responses[] = $this->requestUrl($baseUrl . $url);
+      $responses[] = $this->requestUrl($baseUrl . $url, $cSession);
     }
     return $responses;
   }
 
-  protected function requestUrl(string $url) {
-    $response = $this->client->withOptions(["verify_peer"=>false,"verify_host"=>false])->request('GET', $url);
-    $statusCode = $response->getStatusCode();
+  protected function requestUrl(string $url, $cSession = null) {
+//    $response = $this->client->withOptions(["verify_peer"=>false,"verify_host"=>false])->request('GET', $url);
+//    $statusCode = $response->getStatusCode();
+//    $title = $response->getInfo();
 //    $content = $response->getContent();
     //$complete = $response->toArray($content);
-    return $response;
+    curl_setopt($cSession, CURLOPT_URL, $url);
+    $html = curl_exec($cSession);
+
+    $info = curl_getinfo($cSession);
+    return $info;
+  }
+
+  private function get_html_title($html) {
+    preg_match("/\<title.*\>(.*)\<\/title\>/isU", $html, $matches);
+    return $matches[1];
+  }
+
+  private function startCurlSession(){
+    if($this->isCurlInstalled()) {
+      $cSession = curl_init();
+      curl_setopt($cSession, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($cSession, CURLOPT_HEADER, FALSE);
+      curl_setopt($cSession, CURLOPT_SSL_VERIFYPEER, FALSE);
+      return $cSession;
+    }
+  }
+
+  private function isCurlInstalled() {
+    if  (in_array  ('curl', get_loaded_extensions())) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
