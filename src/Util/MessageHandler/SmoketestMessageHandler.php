395af69bbf6fa427a46d3f9cea050e5242543f9e
Thomas Moseler <tmoseler@gmx.de>
2025-06-11 12:59:14 +0200

    Reduced the number of methods. This many were not needed :)

--- src/Util/MessageHandler/SmoketestMessageHandler.php
+++ src/Util/MessageHandler/SmoketestMessageHandler.php
@@ -25,28 +25,31 @@ class SmoketestMessageHandler {
     }
   }
 
-  private function checkUrl($url): array {
+  /**
+   * Run a curl request on the url and extract HTTP status code
+   * and error description from HTML title.
+   */
+  public function checkUrl($url): array {
     $cSession = $this->startCurlSession();
     $response = NULL;
     if ($cSession) {
-        $response = $this->requestUrl($url, $cSession);
+      curl_setopt($cSession, CURLOPT_URL, $url);
+      $html = curl_exec($cSession);
+      $info = curl_getinfo($cSession);
+
+      $response = [
+        'title' => $this->get_html_title($html),
+        'status_code' => $info['http_code'],
+        'url' => $url,
+      ];
     }
     return $response;
   }
 
-  private function requestUrl(string $url, $cSession = NULL) {
-    curl_setopt($cSession, CURLOPT_URL, $url);
-    $html = curl_exec($cSession);
-    $info = curl_getinfo($cSession);
-
-    $response = [
-      'title' => $this->get_html_title($html),
-      'status_code' => $info['http_code'],
-      'url' => $url,
-    ];
-    return $response;
-  }
-
+  /**
+   * A regex to extract the HTML Title from the HTML.
+   * In Symfony error messages, this contains an error description.
+   */
   private function get_html_title($html) {
     preg_match("/\<title.*\>(.*)\<\/title\>/isU", $html, $matches);
     if($matches) {
@@ -58,21 +61,14 @@ class SmoketestMessageHandler {
   }
 
   private function startCurlSession() {
-    if ($this->isCurlInstalled()) {
+    // Check if CURL is installed, else return FALSE.
+    if (in_array('curl', get_loaded_extensions())) {
       $cSession = curl_init();
       curl_setopt($cSession, CURLOPT_RETURNTRANSFER, TRUE);
       curl_setopt($cSession, CURLOPT_HEADER, FALSE);
       curl_setopt($cSession, CURLOPT_SSL_VERIFYPEER, FALSE);
       curl_setopt($cSession, CURLOPT_FOLLOWLOCATION, TRUE);
       return $cSession;
-    } else {
-      return FALSE;
-    }
-  }
-
-  private function isCurlInstalled() {
-    if (in_array('curl', get_loaded_extensions())) {
-      return TRUE;
     } else {
       return FALSE;
     }
