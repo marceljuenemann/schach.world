8d7c99b0a4b28679c9c76ad67f9764d6a6b26346
Marcel Juenemann <mail@marcel-juenemann.de>
2025-08-20 16:03:03 +0100

    Replace legacy navigation sidebar with frontpage sidebar

--- public/wordpress/wp-content/themes/nsv2020/sidebar/navigation-sidebar.php
+++ public/wordpress/wp-content/themes/nsv2020/sidebar/navigation-sidebar.php
@@ -1,21 +1,3 @@
 <?php
-  require(ABSPATH . '../verband/index.menu.php' );
-  foreach ( $nsvMenu as $label => $menuitem ) {
-    $label = utf8_encode($label);
-    echo "<div class='card shadow nsv-card nsv-sidebar-card'>";
-    echo "<div class='card-body'>";
-    echo "<h5 class='card-title'>$label</h5>";
 
-    foreach ( $menuitem as $sublabel => $url ) {
-      if ( !is_numeric ( $sublabel ) )
-      {
-        $sublabel = utf8_encode($sublabel);
-        if ( substr ( $url, 0, 4 ) == "http" )
-            echo "<a href=\"$url\">$sublabel</a><br>";
-        else
-            echo "<a href=\"/$url\">$sublabel</a><br>";
-      }
-    }
-    
-    echo "</div></div>";
-  }
+dynamic_sidebar( 'frontpage_sidebar' );
