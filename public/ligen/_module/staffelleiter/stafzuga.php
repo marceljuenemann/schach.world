cc7cbae55ddf8605ec2ba67fca33c6576caf7baa
Marcel Juenemann <mail@marcel-juenemann.de>
2025-12-14 16:36:30 +0000

    Prepared Queries: Migrate Spielberechtigung and Zugangsdaten

--- public/ligen/_module/staffelleiter/stafzuga.php
+++ public/ligen/_module/staffelleiter/stafzuga.php
@@ -10,30 +10,29 @@
  * @subpackage staffelleiter
  */
 
-    require_once ( "login.inc.php" );
-    require_once ( "tinyurl.inc.php" );
+  require_once ( "login.inc.php" );
+  require_once ( "tinyurl.inc.php" );
 
 	// Anfang
 	echo "Mit den folgenden Links k&ouml;nnen die jeweiligen Paarungen in das System eingegeben werden. Sollte die Eingabe durch Mannschaftsf&uuml;hrer bei Ihrem Turnier aktiviert sein, so erhalten die Mannschaftsf&uuml;hrer diese Links automatisch drei Tage vor einem Spieltag. Sollte jemand Probleme haben, Mails vom System zu empfangen, so k&ouml;nnen Sie ihm seinen Link direkt aush&auml;ndigen.<br />";
 
 	// Iteration über alle Paarungen
-	$rsrc = mysql_query ( "SELECT id, runde, mannschaft1, mannschaft2 FROM paarungen WHERE staffel=$admin[staffel] ORDER BY runde", $globals ['db'] );
-    $runde = 0;
-	while ( $paarung = mysql_fetch_array ( $rsrc, MYSQL_ASSOC ) )
+	$rsrc = SED_Query ( "SELECT id, runde, mannschaft1, mannschaft2 FROM paarungen WHERE staffel=? ORDER BY runde", [$admin['staffel']])->fetchAllAssociative();
+  $runde = 0;
+	foreach ( $rsrc as $paarung )
 	{
-        // Neue Runde?
-        if ( $paarung ['runde'] != $runde ) {
-            $runde = $paarung ['runde'];
-            echo "<br /><b>Spieltag $runde:</b><br />";
-        }
+    // Neue Runde?
+    if ( $paarung ['runde'] != $runde ) {
+      $runde = $paarung ['runde'];
+      echo "<br /><b>Spieltag $runde:</b><br />";
+    }
         
 		// Sicherheitsstring holen, evtl. neuen
-        $link = SED_TINYURL_Paarung ( $paarung ['id'] );
+    $link = SED_TINYURL_Paarung ( $paarung ['id'] );
 		
 		// Ausgeben
 		echo $globals['teams'][$paarung ['mannschaft1']]." - ";
-        echo $globals['teams'][$paarung ['mannschaft2']].": ";
-        echo "<a href='$link'>$link</a><br />";
+    echo $globals['teams'][$paarung ['mannschaft2']].": ";
+    echo "<a href='$link'>$link</a><br />";
 	}	  
 	echo "<br />";
-?>
