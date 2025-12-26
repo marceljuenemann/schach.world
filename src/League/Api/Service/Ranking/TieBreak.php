b8bbde60259c35332829abcadd4a3bac0a20c30f
Marcel Juenemann <mail@marcel-juenemann.de>
2025-12-26 10:10:30 +0100

    Ranking: Create TieBreak and Score classes

---
+++ src/League/Api/Service/Ranking/TieBreak.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nsv\League\Chess\Ranking;
+
+use Nsv\League\Entity\Team;
+
+/**
+ * A TieBreak for sorting the ranking.
+ */
+abstract class TieBreak {
+
+  /**
+   * Comparator for two teams in the ranking. May have side effects.
+   */
+  abstract public function compare(Team $a, Team $b): int;
+}
