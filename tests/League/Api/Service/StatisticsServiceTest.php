56175634a1d033b036a9e197f4f2a9890c5cdfb9
Thomas Moseler <tmoseler@gmx.de>
2026-03-11 17:11:22 +0100

    Starting Tests for the data calculation methods of StatisticsService. Deleting the Snapshot Test for HTML output of the statistics page, since it is replaced by the new tests.

---
+++ tests/League/Api/Service/StatisticsServiceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace League\Api\Service;
+
+use Nsv\League\Api\Service\StatisticsService;
+use Nsv\League\Entity\Division;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Spatie\Snapshots\MatchesSnapshots;
+use Tests\League\LeagueTestCase;
+
+class StatisticsServiceTest extends LeagueTestCase {
+  use MatchesSnapshots;
+
+  private StatisticsService $statisticsService;
+
+  protected function setUp(): void {
+    parent::setUp();
+    $this->statisticsService = $this->container->get(StatisticsService::class);
+  }
+
+  /**
+   * Test for first data method for the 'DWZ-Statistik' table.
+   * Testing regular divisions with no special cases.
+   */
+  #[DataProvider('divisionDataProvider')]
+  public function testTeamsDwzCalculation($league, $division): void {
+    $division = $this->division($league, $division);
+    $teams_with_active_players = $this->statisticsService->teams_with_active_players($division);
+    $active_teams_with_players = $this->statisticsService->active_teams_with_players($teams_with_active_players, $division);
+    $dwzCalculationData = $this->statisticsService->teams_dwz_calculation($active_teams_with_players, $division);
+    $this->assertMatchesSnapshot($dwzCalculationData);
+  }
+
+  public static function divisionDataProvider(): \Generator {
+    yield 'Bezirk Hannover Kreisliga Ost 17/18' => ['bezirk1-1718', 'kreisliga-ost'];
+    yield 'Bezirk Hannover Bezirksliga 18/19' => ['bezirk1-1819', 'bezirksliga'];
+    yield 'Bezirk 3 Bezirksklasse 21/22' => ['bezirk3-2122', 'bezirksklasse'];
+    yield 'Landesliga Süd 21/22' => ['nsv-2122', 'landesliga-sued'];
+    yield 'Verbandsliga Nord 22/23' => ['nsv-2223', 'verbandsliga-nord'];
+  }
+
+  private function division(string $leaguePath, string $divisionPath): Division {
+    $league = $this->leagueRepository->findByPathOrPrefix($leaguePath);
+    return $league->divisionByPath($divisionPath);
+  }
+}
