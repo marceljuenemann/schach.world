14a7b066be67a79b235dc34d75808e4164a55196
Thomas Moseler <tmoseler@gmx.de>
2026-03-11 17:46:46 +0100

    Snapshot-Tests for all Data calculation methods in StatisticsService that are collected and sent to the template in DivisionController.

--- tests/League/Api/Service/StatisticsServiceTest.php
+++ tests/League/Api/Service/StatisticsServiceTest.php
@@ -19,7 +19,7 @@ class StatisticsServiceTest extends LeagueTestCase {
   }
 
   /**
-   * Test for first data method for the 'DWZ-Statistik' table.
+   * Snapshot-Test for first data method for the 'DWZ-Statistik' table.
    * Testing regular divisions with no special cases.
    */
   #[DataProvider('divisionDataProvider')]
@@ -27,8 +27,55 @@ class StatisticsServiceTest extends LeagueTestCase {
     $division = $this->division($league, $division);
     $teams_with_active_players = $this->statisticsService->teams_with_active_players($division);
     $active_teams_with_players = $this->statisticsService->active_teams_with_players($teams_with_active_players, $division);
-    $dwzCalculationData = $this->statisticsService->teams_dwz_calculation($active_teams_with_players, $division);
-    $this->assertMatchesSnapshot($dwzCalculationData);
+    $dwzTeamsCalculationData = $this->statisticsService->teams_dwz_calculation($active_teams_with_players, $division);
+    $this->assertMatchesSnapshot($dwzTeamsCalculationData);
+  }
+
+  /**
+   * Snapshot-Test for second data method for the 'DWZ-Statistik' table.
+   * Testing regular divisions with no special cases.
+   */
+  #[DataProvider('divisionDataProvider')]
+  public function testDwzStatisticsAdditionalData($league, $division): void {
+    $division = $this->division($league, $division);
+    $teams_with_active_players = $this->statisticsService->teams_with_active_players($division);
+    $active_teams_with_players = $this->statisticsService->active_teams_with_players($teams_with_active_players, $division);
+    $dwzStatisticsAdditionalData = $this->statisticsService->dwz_statistics_additional_data($active_teams_with_players, $division);
+    $this->assertMatchesSnapshot($dwzStatisticsAdditionalData);
+  }
+
+  /**
+   * Snapshot-Test for data method for the 'Topscorer' table.
+   * Testing regular divisions with no special cases.
+   */
+  #[DataProvider('divisionDataProvider')]
+  public function testCalculateTopscorer($league, $division): void {
+    $division = $this->division($league, $division);
+    $calculateTopscorerData = $this->statisticsService->calculate_topscorer($division);
+    $this->assertMatchesSnapshot($calculateTopscorerData);
+  }
+
+  /**
+   * Snapshot-Test for first data method for the 'Spiel-Statistik' table.
+   * Testing regular divisions with no special cases.
+   */
+  #[DataProvider('divisionDataProvider')]
+  public function testTeamGameScoreData($league, $division): void {
+    $division = $this->division($league, $division);
+    $active_teams_with_parings = $this->statisticsService->active_teams_with_parings($division);
+    $teamGameScoreData = $this->statisticsService->team_game_score_data($active_teams_with_parings);
+    $this->assertMatchesSnapshot($teamGameScoreData);
+  }
+
+  /**
+   * Snapshot-Test for second data method for the 'Spiel-Statistik' table.
+   * Testing regular divisions with no special cases.
+   */
+  #[DataProvider('divisionDataProvider')]
+  public function testTeamGameScoreAdditionalData($league, $division): void {
+    $division = $this->division($league, $division);
+    $teamGameScoreAdditionalData = $this->statisticsService->team_game_score_additional_data($division);
+    $this->assertMatchesSnapshot($teamGameScoreAdditionalData);
   }
 
   public static function divisionDataProvider(): \Generator {
